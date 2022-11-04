7803530c7ad6a7995ece62e05b3656cddf34b600
astronights <shubhankar.a31@gmail.com>
2022-11-05 00:08:42 +0800

    Skip connection CNN, to fix

--- model/baseline_cnn_2.py
+++ model/baseline_cnn_2.py
@@ -1,58 +1,56 @@
 import torch.nn as nn
+import torch.nn.functional as F
 from utils import device_utils
+
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(Block, self).__init__()
+
+        self.skip = nn.Sequential()
+
+        if stride != 1 or in_channels != out_channels:
+          self.skip = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride, bias=False),
+            nn.BatchNorm2d(out_channels))
+        else:
+          self.skip = None
+
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1, bias=False),
+            )
+        
+        self.bn = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        out = self.block(x)
+
+        if self.skip is not None:
+          out = self.skip(x)
+        else:
+          out = x
+
+        out += x
+
+        out = F.relu(out)
+        out = self.bn(out)
+        return out
 
 class BaselineCNN2(nn.Module):
 
     def __init__(self, num_classes):
         super(BaselineCNN2, self).__init__()
 
-        self.features = [
-            nn.Conv2d(3, 16, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 16, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(16),
-            nn.MaxPool2d(2, 2),
-
-            nn.Conv2d(16, 32, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 32, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(2, 2),
-
-            nn.Conv2d(32, 64, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 64, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2, 2),
-            
-            nn.Conv2d(64, 128, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(128),
-            nn.Conv2d(128, 128, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(2, 2),
-
-            nn.Conv2d(128, 256, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(256),
-            nn.Conv2d(256, 256, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(256),
-            nn.MaxPool2d(2, 2)
-        ]
-
-        self.device = device_utils.get_device()
-
-        for i, layer in enumerate(self.features):
-            self.features[i] = layer.to(self.device)
-
+        self.features = nn.Sequential(
+            Block(3, 64, stride=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            Block(64, 128, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            Block(128, 256, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
 
         
         self.aap = nn.AdaptiveAvgPool2d((1,1))
@@ -62,19 +60,7 @@ class BaselineCNN2(nn.Module):
     
     def forward(self, x):
 
-        # Adding skip connection
-        activation_count = 0
-        skipped = x
-        out = x
-        for layer in self.features:
-            if(isinstance(layer, nn.ReLU)):
-                activation_count += 1
-                if(activation_count % 2 == 0):
-                    out = out + skipped
-            if(isinstance(layer, nn.MaxPool2d)):
-                skipped = out
-            out = layer(out)
-            
+        x = self.features(x)
             
         out = self.aap(out)
         out = self.flatten(out)
