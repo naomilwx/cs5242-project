4dec418e0ab32777e89d7765546e2614014e32a3
Naomi Leow <naomileow@Naomis-MacBook-Pro.local>
2022-11-05 11:40:50 +0800

    Fix skip connections, modify baseline 2 to be more similar to baseline 1

--- model/baseline_cnn_2.py
+++ model/baseline_cnn_2.py
@@ -6,33 +6,24 @@ class Block(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super(Block, self).__init__()
 
-        self.skip = nn.Sequential()
-
-        if stride != 1 or in_channels != out_channels:
-          self.skip = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride, bias=False),
-            nn.BatchNorm2d(out_channels))
-        else:
-          self.skip = None
+        self.skip = nn.Sequential(
+          nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride, bias=False),
+          nn.BatchNorm2d(out_channels))
 
         self.block = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=stride, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1, bias=False),
-            )
+            nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=stride, bias=False),
+          )
         
         self.bn = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
+        skip = self.skip(x)
+        
         out = self.block(x)
-
-        if self.skip is not None:
-          out = self.skip(x)
-        else:
-          out = x
-
-        out += x
+        out += skip
 
         out = F.relu(out)
         out = self.bn(out)
@@ -44,24 +35,23 @@ class BaselineCNN2(nn.Module):
         super(BaselineCNN2, self).__init__()
 
         self.features = nn.Sequential(
-            Block(3, 64, stride=1),
+            Block(3, 16),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            Block(64, 128, stride=2),
+            Block(16, 32),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            Block(128, 256, stride=2),
+            Block(32, 64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            Block(64, 128),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
-
         
         self.aap = nn.AdaptiveAvgPool2d((1,1))
         self.flatten = nn.Flatten()
-        self.classifier = nn.Linear(256, num_classes)
+        self.classifier = nn.Linear(128, num_classes)
 
     
     def forward(self, x):
-
-        x = self.features(x)
-            
+        out = self.features(x)
         out = self.aap(out)
         out = self.flatten(out)
         out = self.classifier(out)
