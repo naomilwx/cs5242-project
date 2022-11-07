57db2ef71931f13a652cbc919ed2ce967470a2eb
Naomi Leow <naomileow@Naomis-MacBook-Pro.local>
2022-11-07 23:32:03 +0800

    Add attention model

--- model/attention_cnn.py
+++ model/attention_cnn.py
@@ -1,43 +1,11 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-class ProjectorBlock(nn.Module):
-    def __init__(self, in_features, out_features):
-        super(ProjectorBlock, self).__init__()
-        self.op = nn.Conv2d(in_channels=in_features, out_channels=out_features,
-            kernel_size=1, padding=0, bias=False)
+from model.attention import SpatialAttention, AttentionConv2d
 
-    def forward(self, x):
-        return self.op(x)
-
-
-class SpatialAttn(nn.Module):
-    def __init__(self, in_features, normalize_attn=True):
-        super(SpatialAttn, self).__init__()
-        self.normalize_attn = normalize_attn
-        self.op = nn.Conv2d(in_channels=in_features, out_channels=1,
-            kernel_size=1, padding=0, bias=False)
-
-    def forward(self, l, g):
-        N, C, H, W = l.size()
-        c = self.op(l+g) # (batch_size,1,H,W)
-        if self.normalize_attn:
-            a = F.softmax(c.view(N,1,-1), dim=2).view(N,1,H,W)
-        else:
-            a = torch.sigmoid(c)
-        g = torch.mul(a.expand_as(l), l)
-        if self.normalize_attn:
-            g = g.view(N,C,-1).sum(dim=2) # (batch_size,C)
-        else:
-            g = F.adaptive_avg_pool2d(g, (1,1)).view(N,C)
-        return c.view(N,1,H,W), g
-    
 class CNNWithAttention(nn.Module):
-
     def __init__(self, num_classes):
         super(CNNWithAttention, self).__init__()
-        # TODO:
         self.features = nn.Sequential(
             nn.Conv2d(3, 16, (3,3), 1, 1),
             nn.ReLU(True),
@@ -61,19 +29,24 @@ class CNNWithAttention(nn.Module):
             nn.Conv2d(64, 64, (3,3), 1, 1),
             nn.ReLU(True),
             nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, (3,3), 1, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(64),
             nn.MaxPool2d(2, 2),
-            
+
             nn.Conv2d(64, 128, (3,3), 1, 1),
             nn.ReLU(True),
             nn.BatchNorm2d(128),
             nn.Conv2d(128, 128, (3,3), 1, 1),
             nn.ReLU(True),
             nn.BatchNorm2d(128),
-            nn.MaxPool2d(2, 2)
+            nn.Conv2d(128, 128, (3,3), 1, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(128),
+            # nn.MaxPool2d(2, 2),
         )
 
-
-        self.attn = SpatialAttn(128)
+        self.attn = SpatialAttention(128)
 
         self.aap = nn.AdaptiveAvgPool2d((1,1))
         self.classifier = nn.Linear(128, num_classes)
@@ -84,7 +57,55 @@ class CNNWithAttention(nn.Module):
         out = self.aap(feat)
 
         _, ct = self.attn(feat, out)
-       
         out = self.classifier(ct)
+
+        return out
+
+class CNNWithConvAttention(nn.Module):
+    def __init__(self, num_classes):
+        super(CNNWithConvAttention, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 16, (3,3), 1, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(16),
+            nn.Conv2d(16, 16, (3,3), 1, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(16),
+            nn.MaxPool2d(2, 2), #84x84
+
+            nn.Conv2d(16, 32, (3,3), 1, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, (3,3), 1, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2, 2), #42x42
+
+            nn.Conv2d(32, 64, (3,3), 1, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, (3,3), 1, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2), # 21x21
+
+            # nn.Conv2d((64, 128, (3, 3), 1, 1),
+            AttentionConv2d(64, 128, 3, 128, 8, 4),
+            nn.ReLU(True),
+            nn.BatchNorm2d(128),
+            AttentionConv2d(128, 128, 3, 128, 8, 4, relative=True, shape=21),
+            nn.ReLU(True),
+            nn.BatchNorm2d(128),
+        )
+
+        self.aap = nn.AdaptiveAvgPool2d((1,1))
+        self.flatten = nn.Flatten()
+        self.classifier = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = self.aap(out)
+        out = self.classifier(self.flatten(out))
 
         return out
