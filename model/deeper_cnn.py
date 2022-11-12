6d130a801cc9e4d77a735856e810a5a4641f1212
Naomi Leow <naomileow@Naomis-MacBook-Pro.local>
2022-11-12 13:24:45 +0800

    Update convblock

--- model/deeper_cnn.py
+++ model/deeper_cnn.py
@@ -7,6 +7,27 @@ class ConvBlock(nn.Module):
 
         blocks = [
             nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(out_channels)
+        ]
+        for i in range(1, depth):
+            blocks.extend([
+                nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1),
+                nn.ReLU(True),
+                nn.BatchNorm2d(out_channels),
+            ])
+        self.block = nn.Sequential(*blocks)
+        
+    def forward(self, x):       
+        return self.block(x)
+
+# We found that ConvBlock performs better - ReLU activation before batchnorm does better
+class ConvBlock2(nn.Module):
+    def __init__(self, in_channels, out_channels, depth=2):
+        super(ConvBlock2, self).__init__()
+
+        blocks = [
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(True),
         ]
@@ -20,6 +41,7 @@ class ConvBlock(nn.Module):
         
     def forward(self, x):       
         return self.block(x)
+
 
 class DeeperCNN(nn.Module):
     def __init__(self, num_classes):
