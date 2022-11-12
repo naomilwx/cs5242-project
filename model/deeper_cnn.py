ffb9b4082f551c130e81fed27a3322b60964150b
Naomi Leow <naomileow@Naomis-MacBook-Pro.local>
2022-11-12 12:53:59 +0800

    Add alternative deeper cnn

--- model/deeper_cnn.py
+++ model/deeper_cnn.py
@@ -1,93 +1,75 @@
 import torch
 import torch.nn as nn
 
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, depth=2):
+        super(ConvBlock, self).__init__()
+
+        blocks = [
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(True),
+        ]
+        for i in range(1, depth):
+            blocks.extend([
+                nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding=1, stride=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(True)
+            ])
+        self.block = nn.Sequential(*blocks)
+        
+    def forward(self, x):       
+        return self.block(x)
+
 class DeeperCNN(nn.Module):
     def __init__(self, num_classes):
         super(DeeperCNN, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 16, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 16, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(16),
-            nn.MaxPool2d(2, 2), # 112x112
-            
-            nn.Conv2d(16, 32, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 32, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(2, 2), # 56x56
-
-            nn.Conv2d(32, 64, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 64, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 64, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2, 2), #28x28
-            
-            nn.Conv2d(64, 128, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(128),
-            nn.Conv2d(128, 128, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(128),
-            nn.Conv2d(128, 128, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(128)
-        )
 
         self.features = nn.Sequential(
-            nn.Conv2d(3, 16, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 16, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(16),
-            nn.MaxPool2d(2, 2), # 112x112
-            
-            nn.Conv2d(16, 32, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 32, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(2, 2), # 56x56
-
-            nn.Conv2d(32, 64, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 64, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2, 2), #28x28
-            
-            nn.Conv2d(64, 128, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(128),
-            nn.Conv2d(128, 128, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(2, 2), #14x14
-
-            nn.Conv2d(128, 256, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(256),
-            nn.Conv2d(256, 256, (3,3), 1, 1),
-            nn.ReLU(True),
-            nn.BatchNorm2d(256),
-            nn.MaxPool2d(2, 2),
+            ConvBlock(3, 16),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            ConvBlock(16, 32),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            ConvBlock(32, 64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            ConvBlock(64, 128),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            ConvBlock(128, 256),
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
         self.aap = nn.AdaptiveAvgPool2d((1,1))
         
         self.classifier = nn.Linear(256, num_classes)
+        self.flatten = nn.Flatten()
+
+    
+    def forward(self, x):
+        out = self.features(x)
+        out = self.flatten(self.aap(out))
+        out = self.classifier(out)
+        return out
+
+
+class DeeperCNN2(nn.Module):
+    def __init__(self, num_classes):
+        super(DeeperCNN, self).__init__()
+        
+        self.features = nn.Sequential(
+            ConvBlock(3, 16, depth=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            ConvBlock(16, 32, depth=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            ConvBlock(32, 64, depth=5),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            ConvBlock(64, 128, depth=5),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+
+        self.aap = nn.AdaptiveAvgPool2d((1,1))
+        
+        self.classifier = nn.Linear(128, num_classes)
         self.flatten = nn.Flatten()
 
     
